b26010aafb909d128d3334d1f3d932daf8be0495
Calvin <calvinkuhnen@gmail.com>
2021-04-11 15:50:01 -0500

    improved error checking, added username update logic

--- backend-carry-app/index.ts
+++ backend-carry-app/index.ts
@@ -13,7 +13,10 @@ const io = new Server(server);
 
 var response: string|null = 'none';
 var instaInfo: any = {};
+var currUname = '';
 
+// Use puppeteer to access instagram graphql query because using axios results
+// in bot detection and a redirect from instagram.
 puppeteer
   .use(StealthPlugin())
   .launch()
@@ -21,18 +24,21 @@ puppeteer
     const page = await browser.newPage();
     await page.goto(config.query);
     await page.waitForTimeout(5000);
-    response = await page.$eval('pre', res => {
-        if (res == null) {
-          return 'response null';
+    response = await page.$eval('pre', res => res.textContent); // get JSON portion of html response
+    if(response != null) {
+      try {
+        instaInfo = JSON.parse(response!);
+        let newUname = instaInfo.data.user.reel.owner.username;
+        if(newUname != currUname) {
+          currUname = newUname;  // check for username update
+          console.log(currUname);
         }
-        else {
-          return res.textContent;
-        }
-    });
-    instaInfo = JSON.parse(response!);
-    console.log(instaInfo.data.user.reel.owner.username);
+      } catch (error) {
+        console.error('Response not JSON error');
+      }
+    }
     await browser.close();
-  })
+  });
 
 //var instaUsername = JSON.parse(response);
 //console.log(instaUsername);
