e766d26950421829a6f4e80238e11658112a6cc8
Calvin <calvinkuhnen@gmail.com>
2021-04-11 02:48:03 -0500

    got puppeteer working for insta username

--- backend-carry-app/index.ts
+++ backend-carry-app/index.ts
@@ -1,8 +1,30 @@
 import express from 'express';
-const app = express();
+import https from 'https';
+import axios from 'axios';
+import puppeteer from 'puppeteer-extra';
+import StealthPlugin from 'puppeteer-extra-plugin-stealth'
+import { Server, Socket } from 'socket.io';
+import config from '../config';
+
 const PORT = process.env.PORT || 5000;
+const app = express();
+const server = https.createServer(app);
+const io = new Server(server);
+
+console.log(config.query);
+
+puppeteer
+  .use(StealthPlugin())
+  .launch()
+  .then(async browser => {
+    const page = await browser.newPage()
+    await page.goto(config.query)
+    await page.waitForTimeout(5000)
+    await page.screenshot({ path: 'stealth.png', fullPage: true })
+    await browser.close()
+  })
 
 app.get('/', (req,res) => res.send('Express + TypeScript Server for Instagram Gallery Viewer'));
 app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+  console.log(`[server]: Server is running at https://localhost:${PORT}`);
 });
