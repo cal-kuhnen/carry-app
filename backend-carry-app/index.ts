ba4299dff590e2096a0553bfcc85380b4c29e010
Calvin <calvinkuhnen@gmail.com>
2021-04-11 22:41:27 -0500

    setup logic to ping username every 30 seconds

--- backend-carry-app/index.ts
+++ backend-carry-app/index.ts
@@ -1,49 +1,66 @@
 import express from 'express';
-import https from 'https';
+import http from 'http';
 import axios from 'axios';
 import puppeteer from 'puppeteer-extra';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth'
 import { Server, Socket } from 'socket.io';
 import config from '../config';
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const app = express();
-const server = https.createServer(app);
+const server = http.createServer(app);
 const io = new Server(server);
 
 var response: string|null = 'none';
 var instaInfo: any = {};
 var currUname = '';
 
-// Use puppeteer to access instagram graphql query because using axios results
-// in bot detection and a redirect from instagram.
-puppeteer
-  .use(StealthPlugin())
-  .launch()
-  .then(async browser => {
-    const page = await browser.newPage();
-    await page.goto(config.query);
-    await page.waitForTimeout(5000);
-    response = await page.$eval('pre', res => res.textContent); // get JSON portion of html response
-    if(response != null) {
-      try {
-        instaInfo = JSON.parse(response!);
-        let newUname = instaInfo.data.user.reel.owner.username;
-        if(newUname != currUname) {
-          currUname = newUname;  // check for username update
-          console.log(currUname);
+io.on("connection", (socket: Socket) => {
+  console.log(socket.id);
+
+  const pingUname = async () => {
+    await new Promise(r => setTimeout(r, 30000));
+    checkUname(socket);
+    pingUname();
+  }
+
+  pingUname();
+
+});
+
+const checkUname = (socket: Socket) => {
+  // Use puppeteer to access instagram graphql query because using axios results
+  // in bot detection and a redirect from instagram.
+  console.log('check uname in progress');
+  puppeteer
+    .use(StealthPlugin())
+    .launch()
+    .then(async browser => {
+      const page = await browser.newPage();
+      await page.goto(config.query);
+      await page.waitForTimeout(5000);
+      response = await page.$eval('pre', res => res.textContent); // get JSON portion of html response
+      if(response != null) {
+        try {
+          instaInfo = JSON.parse(response!);
+          let newUname = instaInfo.data.user.reel.owner.username;
+          if(newUname != currUname) {
+            currUname = newUname;  // check for username update
+            console.log(currUname + 'here');
+            socket.emit('change', currUname);
+          }
+        } catch (error) {
+          console.error(error);
         }
-      } catch (error) {
-        console.error('Response not JSON error');
       }
-    }
-    await browser.close();
-  });
+      await browser.close();
+    });
+}
 
 //var instaUsername = JSON.parse(response);
 //console.log(instaUsername);
 
 app.get('/', (req,res) => res.send('Express + TypeScript Server for Instagram Gallery Viewer'));
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`[server]: Server is running at https://localhost:${PORT}`);
 });
