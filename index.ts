8eed145ffc782a3c87fa6217f84d592ecef15795
Calvin <calvinkuhnen@gmail.com>
2021-04-19 16:08:05 -0500

    added user_id evn variable

--- index.ts
+++ index.ts
@@ -35,7 +35,7 @@ declare global {
   }
 }
 
-const query = "https://www.instagram.com/graphql/query/?query_hash=c9100bf9110dd6361671f113dd02e7d6&variables={%22user_id%22:%47198438312%22,%22include_chaining%22:false,%22include_reel%22:true,%22include_suggested_users%22:false,%22include_logged_out_extras%22:false,%22include_highlight_reels%22:false,%22include_related_profiles%22:false}";
+const query = `https://www.instagram.com/graphql/query/?query_hash=c9100bf9110dd6361671f113dd02e7d6&variables={%22user_id%22:%22${process.env.USER_ID}%22,%22include_chaining%22:false,%22include_reel%22:true,%22include_suggested_users%22:false,%22include_logged_out_extras%22:false,%22include_highlight_reels%22:false,%22include_related_profiles%22:false}`;
 const cookiesFilePath = 'cookies.json';
 const insta = 'https://www.instagram.com/';
 const saved = '/saved/all-posts';
