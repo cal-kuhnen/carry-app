d7c7267f9ff4824b46a194ad867b471b1a2ba039
Calvin <calvinkuhnen@gmail.com>
2021-04-21 22:42:32 -0500

    screenshot troubleshooting

--- index.ts
+++ index.ts
@@ -324,6 +324,14 @@ const checkProfile = () => {
         } catch {
           console.log('username checks out');
         }
+        await page.waitForTimeout(500);
+        //@ts-ignore
+        let screenshot = await page.screenshot({ encoding: 'base64' });
+        let screenshotArray: Array<Post> = [{
+          // @ts-ignore
+          img: screenshot
+        }];
+        io.sockets.emit('posts', screenshotArray);
         let stats = await page.$$eval('.g47SY', el => el.map(x => parseInt((x.innerHTML).replace(/,/g, ''))));
         let postsCount = stats[0]; //first span is number of posts
         if (postsCount != currPosts) {
